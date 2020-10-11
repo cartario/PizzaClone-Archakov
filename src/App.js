1892bf8c9412df84af6c9b37578df68c4fd356f8
VasiliyMac <cartario@yandex.ru>
2020-10-11 10:11:42 +0300

    верстка filter-sort

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 function App() {
+  const [visible, setVisible] = React.useState(false);
+
+  const handlerPopup = () => {
+    if(visible){
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }    
+  }
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -30,7 +40,48 @@ function App() {
           
           </div>
         </header>
-        <main className="main">main
+        <main className="main">
+          <div className="container">
+            <div className="filter-group">
+              <ul className="filter-group__list">              
+                <li className="filter-group__item filter-group__item--active">
+                  Все
+                </li>
+                <li className="filter-group__item">
+                  Мясные
+                </li>
+                <li className="filter-group__item">
+                  Вегетарианская
+                </li>
+                <li className="filter-group__item">
+                  Гриль
+                </li>
+                <li className="filter-group__item">
+                  Острые
+                </li>
+                <li className="filter-group__item">
+                  Закрытые
+                </li>
+              </ul>
+              <div className="filter-group__sort sort">
+                <div className="sort__label">
+                  <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z" fill="#2C2C2C"></path>
+                  </svg>
+                  <b>Сортировка по: </b><span onClick={handlerPopup}>популярности</span>
+                </div>
+
+                {visible &&
+                  <ul className="sort__popup">
+                    <li className="sort__popup--active">популярности</li>
+                    <li>цене</li>
+                    <li>алфавиту</li>
+                  </ul>
+                }           
+                
+              </div>
+            </div>
+          </div>
         </main>
         <footer className="footer">footer
         </footer>
