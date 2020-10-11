3065c664ab94fe5b99bc8066ab81ad19b6a6f0ae
VasiliyMac <cartario@yandex.ru>
2020-10-11 10:59:51 +0300

    верстка pizzas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import React from 'react';
 
+const Pizza = () => {  
+
+  return (
+    <li className="pizza">
+      <img width="250" alt="pizza-img" src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"/>
+      <h3>Чизбургер-пицца</h3>
+      <div className="pizza__options options">
+
+        <ul className="options__types">
+          <li className="options__type options__button options__button--active">тонкое</li>
+          <li className="options__type options__button">традиционное</li>
+        </ul>
+        <ul className="options__sizes">
+          <li className="options__size options__button">26 см.</li>
+          <li className="options__size options__button options__button--active">30 см.</li>
+          <li className="options__size options__button">40 см.</li>
+        </ul>
+
+      </div>
+      <div className="pizza__footer">
+        <p className="pizza__price">от 395 Р.</p>
+        <button className="button">
+          Добавить
+        </button>
+      </div>
+    </li>
+  );
+};
+
 function App() {
   const [visible, setVisible] = React.useState(false);
 
@@ -81,6 +110,14 @@ function App() {
                 
               </div>
             </div>
+            <section className="content-group">
+              <h2>Все пиццы</h2>
+              <ul className="pizzas">                
+                {Array.from('1234567890').map((pizza)=>
+                <Pizza key={pizza}/>
+                )}
+              </ul>
+            </section>
           </div>
         </main>
         <footer className="footer">footer
