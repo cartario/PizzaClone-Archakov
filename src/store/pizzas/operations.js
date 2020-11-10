f1faf6348abfc5527f2bf3a93ef4ec56ee25dee8
VasiliyMac <cartario@yandex.ru>
2020-11-10 14:35:53 +0300

    fake-server

--- src/store/pizzas/operations.js
+++ src/store/pizzas/operations.js
@@ -3,10 +3,10 @@ import { ActionCreators } from './actions';
 export const Operations = {
   fetchPizzas: () => (dispatch) => {    
 
-    fetch('http://localhost:3000/db.json')
+    fetch('http://localhost:3000/pizzas')
       .then((res) => res.json())
       .then((json) => {
-        dispatch(ActionCreators.setPizzas(json.pizzas));
+        dispatch(ActionCreators.setPizzas(json));
         dispatch(ActionCreators.isLoaded(true));
       })
       .catch((err) => {        
