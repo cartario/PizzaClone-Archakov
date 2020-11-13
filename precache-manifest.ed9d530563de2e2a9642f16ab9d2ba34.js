14674bc89b98a8146feb654d7d2924172073e0c8
VasiliyMac <cartario@yandex.ru>
2020-11-13 13:38:51 +0300

    Updates

---
+++ precache-manifest.ed9d530563de2e2a9642f16ab9d2ba34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1db540e3717275086571e7c5dafdc0",
+    "url": "/pizza/index.html"
+  },
+  {
+    "revision": "b8da463c04384bb874b4",
+    "url": "/pizza/static/css/main.7936b2ad.chunk.css"
+  },
+  {
+    "revision": "8ec2e5a2346047a17f06",
+    "url": "/pizza/static/js/2.fadf63da.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/pizza/static/js/2.fadf63da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8da463c04384bb874b4",
+    "url": "/pizza/static/js/main.0736362f.chunk.js"
+  },
+  {
+    "revision": "3273560c08a8e0f0e585",
+    "url": "/pizza/static/js/runtime-main.cc810ad9.js"
+  }
+]);
